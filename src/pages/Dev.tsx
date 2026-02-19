46320a6c784d05067521f0cdf8a062c5a0a3a4f0
unknown <mjmaxbq@icloud.com>
2026-02-19 14:00:43 +0100

    setup dev page for local use when building components

---
+++ src/pages/Dev.tsx
@@ -0,0 +1,17 @@
+// import { useTranslation } from 'react-i18next';
+
+AboutPage.route = {
+  path: '/dev',
+  menuLabel: 'main.navigation.dev',
+  index: 2,
+};
+
+export default function AboutPage() {
+  //   const { t } = useTranslation();
+
+  return (
+    <section>
+      <div>hello</div>
+    </section>
+  );
+}
