17362fc7b90095cc00e15e46c664ad83f07b4430
MarcusHome9 <pirayajk@hotmail.com>
2026-02-19 16:05:57 +0100

    creaded variants for carousel

--- src/stories/ui/BiografCarousel.stories.tsx
+++ src/stories/ui/BiografCarousel.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
+import BiografCard from '@/components/custom/BiografCard';
 import BiografCarousel from '@/components/custom/BiografCarousel';
+import Image from '../../parts/Image';
+import { MemoryRouter, Outlet, Route, Routes } from 'react-router-dom';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -14,7 +17,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    variant: {
+    /*variant: {
       description: 'The variant to use.',
       control: { type: 'select' },
       options: [
@@ -28,15 +31,13 @@ const meta = {
       table: {
         type: { detail: 'string' },
       },
-    },
-    size: {
-      description: 'The size to use.',
+    },*/
+
+    orientation: {
       control: { type: 'select' },
-      options: ['default', 'sm', 'lg', 'icon'],
-      table: {
-        type: { detail: 'string' },
-      },
+      options: ['horizontal', 'vertical'],
     },
+
     children: {
       description: 'In this case it is a title',
       control: 'text',
@@ -45,6 +46,29 @@ const meta = {
       },
     },
   },
+
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Routes>
+          <Route
+            element={
+              <Outlet
+                context={[
+                  { bwImages: false, categoryChoice: '', sortChoice: '' },
+                  () => {},
+                ]}
+              />
+            }
+            path="/"
+          >
+            <Route path="/" element={<Story />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    ),
+  ],
+  
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
   args: { onClick: () => console.log('clicked') },
 } satisfies Meta<typeof BiografCarousel>;
@@ -52,29 +76,32 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Dates: Story = {
   args: {
-    variant: 'default',
+    children: (
+      <div className='flex flex-col'>
+        <span className='justify-center'>15:e Feb</span>
+        <span className='flex justify-center text-xs'>Salong 2</span>
+      </div>
+    ),
+  },
+};
+
+export const Images: Story = {
+  args: {
+    children: <div className=' h-45 rounded-xl m-0 border-0'><Image className='h-45' src="images/products/1.jpg" alt="A group photo of our employees." />
+  </div>
+  },
+};
+
+export const Large: Story = {
+  args: {
     children: 'Button',
-    },
-    };
-    
-    export const Secondary: Story = {
-      args: {
-        children: 'Button',
-        },
-        };
-        
-        export const Large: Story = {
-          args: {
-            size: 'lg',
-            children: 'Button',
-            },
-            };
-            
-            export const Small: Story = {
-              args: {
-                size: 'sm',
-                children: 'Button',
-                },
-                };
+  },
+};
+
+export const Small: Story = {
+  args: {
+    children: 'Button',
+  },
+};
