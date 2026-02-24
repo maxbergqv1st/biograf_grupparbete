e69908ba2565eef73dbb977037cc81668a2a9c7f
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2026-02-24 15:35:21 +0100

    Add scripts for the be to package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 import { useLocation } from 'react-router-dom';
 
-import type { Movie } from './interfaces/Interfaces';
-import Footer from './partials/Footer';
-import Header from './partials/Header';
-import Main from './partials/Main';
-import useFetchJson from './utilities/useFetchJson';
+import RootLayout from '@/components/RootLayout';
 
 export default function App() {
-  // scroll to top when the route changes
-  useLocation();
-  window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  const { pathname } = useLocation();
 
-  const [movies] = useFetchJson<Movie[]>('/api/movie');
-  const [showMovies, setShowMovies] = useState(false);
+  // Scroll to top on every route change
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [pathname]);
 
-  return (
-    <>
-      <Header />
-      <Main />
-      <button onClick={() => setShowMovies(!showMovies)}>
-        {showMovies ? 'Hide Movies' : 'Show Movies'}
-      </button>
-      {showMovies && movies && (
-        <section>
-          {movies.map((movie) => (
-            <article key={movie.id}>
-              <h3>{movie.title}</h3>
-              <p>{movie.description_short}</p>
-              <hr />
-            </article>
-          ))}
-        </section>
-      )}
-      <Footer />
-    </>
-  );
+  return <RootLayout />;
 }
