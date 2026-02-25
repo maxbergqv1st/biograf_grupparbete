3e474f6773b977048cf36cc095d82695f5c901f3
unknown <mjmaxbq@icloud.com>
2026-02-25 09:20:08 +0100

    added DevPage changes

--- src/pages/DevPage.tsx
+++ src/pages/DevPage.tsx
@@ -1,7 +1,7 @@
 import type Movie from '@/interfaces/Movie';
 import { useLoaderData } from 'react-router-dom';
 
-import MovieCard from '@/components/custom/MovieCard';
+import BiografCard from '@/components/custom/BiografCard';
 
 import moviesLoader from '@/utils/movieLoader';
 
@@ -16,9 +16,16 @@ export default function DevPage() {
   const movies = useLoaderData<Movie[]>();
 
   return (
-    <>
-      <h1>Max och Dusans Dev Page</h1>
-      <MovieCard movie={movies} />
-    </>
+    <section className="grid gap-4 md:grid-cols-2">
+      {movies.map((movie) => (
+        <BiografCard
+          key={movie.id}
+          title={movie.title}
+          description={`Director: ${movie.director}`}
+        >
+          <p>{movie.description_short}</p>
+        </BiografCard>
+      ))}
+    </section>
   );
 }
