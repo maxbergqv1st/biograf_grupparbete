2193b01e988c687eb8f92d2de83b69edccb80caa
Marcusjk9 <marcus.jakobsson@student.nbi-handelsakademin.se>
2026-03-13 12:06:59 +0100

    conflict resolve

--- backend/src/Server.cs
+++ backend/src/Server.cs
@@ -20,11 +20,8 @@ public static class Server
             $"Password={config.GetProperty("password").ToString()};";
         builder.Services.AddMySqlDataSource(connectionString);
         builder.Services.AddScoped<IMovieRepository, MovieRepository>();
-<<<<<<< HEAD
         builder.Services.AddScoped<IScreeningRepository, ScreeningRepository>();
-=======
         builder.Services.AddScoped<ICloudinaryRepository, CloudinaryRepository>();
->>>>>>> origin/dev
         App = builder.Build();
         if (App.Environment.IsDevelopment())
         {
@@ -37,16 +34,13 @@ public static class Server
         ErrorHandler.Start();
         FileServer.Start();
         LoginRoutes.Start();
-        
+
         Session.Start();
 
         App.MapMovieEndpoints();
-<<<<<<< HEAD
         App.MapScreeningEndpoints();
-=======
         App.MapCloudinaryEndpoints();
         RestApi.Start();
->>>>>>> origin/dev
         // Start the server on port 5001
         var runUrl = "http://localhost:" + Globals.port;
         Log("Server running on:", runUrl);
