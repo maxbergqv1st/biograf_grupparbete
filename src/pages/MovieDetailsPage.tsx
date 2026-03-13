50134d38bbfb8c9ad87c5e52dc3c8f7635a2360f
Marcusjk9 <marcus.jakobsson@student.nbi-handelsakademin.se>
2026-03-13 11:49:28 +0100

    changed posision on trailer

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -1,10 +1,10 @@
 import type { ComponentProps } from 'react';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 import { useMovie } from '@/api/hooks/useMovies';
 import { useScreenings } from '@/api/hooks/useScreenings';
 import { Trailer } from '@/stories/ui/BiografButton.stories';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 import BiografButton from '@/components/custom/BiografButton';
@@ -27,9 +27,8 @@ export default function MovieDetailsPage() {
   const { id } = useParams();
   const movieId = Number(id);
   const { data, isLoading } = useMovie(movieId);
-  const { data: screeningsData, isLoading: screeningsLoading } = useScreenings(
-    movieId,
-  );
+  const { data: screeningsData, isLoading: screeningsLoading } =
+    useScreenings(movieId);
   console.log(data);
   console.log(isLoading);
   console.log(id);
@@ -62,7 +61,7 @@ export default function MovieDetailsPage() {
             key={date}
             type="button"
             onClick={() => setSelectedDate(date)}
-            className="flex flex-col justify-center items-center h-full w-full rounded-xl"
+            className="flex h-full w-full flex-col items-center justify-center rounded-xl"
           >
             <span>
               {new Intl.DateTimeFormat('sv-SE', { day: 'numeric' }).format(
@@ -92,7 +91,7 @@ export default function MovieDetailsPage() {
                 }
                 type="button"
                 onClick={() => setSelectedTime(screening.screeningTime ?? null)}
-                className="flex flex-col justify-center items-center h-full w-full rounded-xl"
+                className="flex h-full w-full flex-col items-center justify-center rounded-xl"
               >
                 <span>{screening.screeningTime?.slice(0, 5) ?? '-'}</span>
                 <span className="text-xs">{screening.hallName ?? ''}</span>
@@ -101,14 +100,13 @@ export default function MovieDetailsPage() {
         : [<span key="select-date">Välj ett datum</span>];
   return (
     <div className="relative left-1/2 grid h-dvh w-screen -translate-x-1/2 grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3 lg:gap-6 lg:px-8">
-
       <div className="h-full w-full">
         <Image
-          className="overflow-hidden h-full w-full object-cover"
+          className="h-dvh w-full object-cover"
           src="/images/movies/grimsby.jpg"
         />
       </div>
-      <AspectRatio className="max-h-full overflow-hidden h-4/3 w-full rounded-lg bg-[#1E1E1E] p-4">
+      <AspectRatio className="h-dvh w-full rounded-lg bg-[#1E1E1E] p-4">
         <h3 className="flex justify-center p-10 text-xl">Välj Datum & tid</h3>
         <Separator />
         <div className="flex justify-center p-5">
@@ -130,41 +128,58 @@ export default function MovieDetailsPage() {
           />
         </div>
         <Separator className="flex justify-center" />
-        <div className="flex justify-center p-20">
-        <BiografButton>
-          <Link to="/booking">- Välj säten -</Link>
-        </BiografButton>
+        <div className="flex justify-center p-8">
+          <BiografButton>
+            <Link to="/booking">- Välj säten -</Link>
+          </BiografButton>
         </div>
       </AspectRatio>
-
-      <AspectRatio className="max-h-full overflow-hidden self-top w-full max-w-md justify-self-center rounded-lg bg-[#1E1E1E] p-4">
-        <h1 className="flex justify-center p-5 text-2xl text-[#b69852]">
-          {data?.data.title ? data?.data.title : 'No title'}
-        </h1>
-        <Separator className="flex justify-center" />
-        <div className='pt-0.5 pb-1.5'>
-        <span>
-          | {data?.data.ageRating + '+' ? data?.data.ageRating + '+': 'No age rating'} |
-          | {data?.data.language?.code ? data?.data.language.code : 'No language'} | 
-          | {Array.isArray(data?.data.genres) ? data?.data.genres.join(', ') : data?.data.genres ? data?.data.genres : 'No genres'} |
-        </span>
-        </div>
-        <Separator className="flex justify-center" />
-        <span className="flex justify-center p-5">
-          {data?.data.description ? data?.data.description : 'No description'}
-        </span>
-        <Separator className="flex justify-center" />
-        <span className="flex justify-center p-5">
-          Director: {data?.data.director ? data?.data.director : 'No director'}
-        </span>
-      </AspectRatio>
-      <AspectRatio className="max-h-full overflow-hidden h-40 w-full rounded-lg border bg-[url(/images/movies/grimsby.jpg)] bg-center">
-        <div className="absolute right-5 bottom-5">
-          <a href="https://www.youtube.com/watch?v=_YtclB_02wA" target="_blank">
-            <BiografButton {...argsTrailer} />
-          </a>
-        </div>
-      </AspectRatio>
+      <div className="flex flex-col gap-4 lg:col-start-3 lg:row-span-3 lg:row-start-1">
+        <AspectRatio className="self-top max-h-full w-full max-w-md justify-self-center rounded-lg bg-[#1E1E1E] p-4">
+          <h1 className="flex justify-center p-5 text-2xl text-[#b69852]">
+            {data?.data.title ? data?.data.title : 'No title'}
+          </h1>
+          <Separator className="flex justify-center" />
+          <div className="pt-0.5 pb-1.5">
+            <span>
+              |{' '}
+              {data?.data.ageRating + '+'
+                ? data?.data.ageRating + '+'
+                : 'No age rating'}{' '}
+              | |{' '}
+              {data?.data.language?.code
+                ? data?.data.language.code
+                : 'No language'}{' '}
+              | |{' '}
+              {Array.isArray(data?.data.genres)
+                ? data?.data.genres.join(', ')
+                : data?.data.genres
+                  ? data?.data.genres
+                  : 'No genres'}{' '}
+              |
+            </span>
+          </div>
+          <Separator className="flex justify-center" />
+          <span className="flex justify-center p-5">
+            {data?.data.description ? data?.data.description : 'No description'}
+          </span>
+          <Separator className="flex justify-center" />
+          <span className="flex justify-center p-5">
+            Director:{' '}
+            {data?.data.director ? data?.data.director : 'No director'}
+          </span>
+        </AspectRatio>
+        <AspectRatio className="h-40 max-h-full w-full rounded-lg border bg-[url(/images/movies/grimsby.jpg)] bg-center">
+          <div className="absolute right-5 bottom-5">
+            <a
+              href="https://www.youtube.com/watch?v=_YtclB_02wA"
+              target="_blank"
+            >
+              <BiografButton {...argsTrailer} />
+            </a>
+          </div>
+        </AspectRatio>
+      </div>
     </div>
   );
 }
