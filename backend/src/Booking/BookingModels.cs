a2a9c232873628284341cbe7ce20fa65cf291dec
NonSentientExistence <david@venngren.se>
2026-03-17 17:51:26 +0100

    Added models for booking

--- backend/src/Booking/BookingModels.cs
+++ backend/src/Booking/BookingModels.cs
@@ -1,10 +1,25 @@
 namespace WebApp.Bookings;
 
-
 public record BookingDto(
     int Id,
     string Email,
     int ScreeningId,
     decimal Total_price,
-    int User_Id
+    int? User_Id,
+    string BookingReference,
+    string Status
+);
+
+public record CreateBookingDto(
+    string Email,
+    int ScreeningId,
+    decimal Total_price,
+    int? User_Id,
+    List<BookingSeatDto> Seats
+);
+
+public record BookingSeatDto(
+    int SeatId,
+    int PriceCategorySeatId,
+    decimal FinalPrice
 );
