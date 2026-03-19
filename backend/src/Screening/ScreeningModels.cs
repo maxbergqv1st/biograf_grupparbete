97492c0c019a77805b0bb62e1fc65f3d4d2267b0
github username <dusan.todorovic@student.nbi-handelsakademin.se>
2026-03-19 12:42:24 +0100

    resolving small issues

--- backend/src/Screening/ScreeningModels.cs
+++ backend/src/Screening/ScreeningModels.cs
@@ -10,18 +10,6 @@ public record ScreeningDto(
     DateOnly ScreeningDate,
     TimeOnly ScreeningTime
 );
-/*
-public record BookingDto(
-    int Id,
-    string Email,
-    int ScreeningId,
-    string ScreeningDate,
-    decimal TotalPrice,
-    string Status,
-    string Reference,
-    int UserId
-);
-*/
 
 
 public record CreateScreeningDto(
